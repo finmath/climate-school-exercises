fb19151b272a6d6a3fe379d6931506d30296c414
Christian Fries <email@christian-fries.de>
2024-10-03 22:12:39 +0200

    Added a Test in the package begin.

---
+++ src/main/java/net/finmath/climateschool/begin/Test.java
@@ -0,0 +1,14 @@
+package net.finmath.climateschool.begin;
+
+/**
+ * Run this class to test your setup.
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Looks as if it works.");
+
+	}
+
+}

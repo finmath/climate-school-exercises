e1aadfa93817c775ae8fd70967e3353a726745c8
Christian Fries <email@christian-fries.de>
2025-10-02 14:05:58 +0200

    Added some more output.

--- src/main/java/net/finmath/climateschool/begin/Test.java
+++ src/main/java/net/finmath/climateschool/begin/Test.java
@@ -7,6 +7,8 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		System.out.println("\nThis test program prints a text - nothing more. This is a basic check to see if the Java Runtime works.");
+
 		System.out.println("\n\tLooks as if it works!");
 
 	}
--- src/main/java/net/finmath/climateschool/begin/Test2.java
+++ src/main/java/net/finmath/climateschool/begin/Test2.java
@@ -10,15 +10,17 @@ public class Test2 {
 
 	public static void main(String[] args) {
 
+		System.out.println("\nThis test program uses a library (finmath lib) to calculate the expectation of a random variable.");
+
 		RandomVariable randomVariable = new RandomVariableFromDoubleArray(0.0, new double[] { 1.0, 0.0, 0.0, 1.0 });
 		
 		double expectation = randomVariable.expectation().doubleValue();
 		
 		if(expectation == 0.5) {
-			System.out.println("\n\tLooks as if it works!");
+			System.out.println("\n\tLooks as if this test works!");
 		}
 		else {
-			System.out.println("\n\tExpectation does not match.");
+			System.out.println("\n\tExpectation does not match - this should not happen.");
 		}
 			
 	}

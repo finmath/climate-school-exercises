496d35d4c2c84d39d843375c94dfed2f4a3f64c2
Christian Fries <email@christian-fries.de>
2025-10-02 19:24:45 +0200

    Building binary images.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module net.finmath.climateschool {
 
-	exports net.finmath.climateschool.other;
+	exports net.finmath.climateschool.ui;
 
 	requires net.finmath.lib;
 	requires net.finmath.plots;
+
 	requires transitive javafx.controls;
 	requires javafx.graphics;
 }

0d4da715ea308da790904a653c6ad1c67da95e77
Christian Fries <email@christian-fries.de>
2025-10-02 14:19:53 +0200

    Added JavaFX.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module net.finmath.climateschool {
+
+	exports net.finmath.climateschool.other;
+
+	requires net.finmath.lib;
+	requires net.finmath.plots;
+	requires transitive javafx.controls;
+	requires javafx.graphics;
+}
